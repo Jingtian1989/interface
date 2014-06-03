bf38bffccdd175b1e620732d40480c2cda20fff0
jingtian1989 <zjt415991957@qq.com>
2014-06-03 16:47:32 +0800

    Update

--- array/array.h
+++ array/array.h
@@ -4,14 +4,14 @@
 #define T array_t
 typedef struct T *T;
 
-extern T 	array_new		(int length, int size);
-extern void array_free		(T *array);
-extern int 	array_length	(T array);
-extern int 	array_size		(T array);
-extern void *array_get		(T array, int i);
-extern void *array_put		(T array, int i, void *elem);
-extern void array_resize	(T array, int length);
-extern T 	array_copy		(T array, int length);
+extern T array_new(int length, int size);
+extern void array_free(T *array);
+extern int array_length(T array);
+extern int array_size(T array);
+extern void *array_get(T array, int i);
+extern void *array_put(T array, int i, void *elem);
+extern void array_resiz(T array, int length);
+extern T array_copy(T array, int length);
 
 #undef T
 #endif
