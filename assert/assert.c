cdcdd536a241c618333f44b49d26eb34171e1358
jingtian1989 <zjt415991957@qq.com>
2014-06-04 11:20:42 +0800

    Update

--- assert/assert.c
+++
@@ -1,8 +0,0 @@
-#include "assert.h"
-
-const except_t assert_failed = {"assert failed"};
-
-void (assert)(int e)
-{
-	assert(e);
-}
--- assert/assert.h
+++
@@ -1,7 +0,0 @@
-#undef assert
-#ifdef NDEBUG
-#define assert(e) ((void)(0))
-#include "except.h"
-extern void assert(int e);
-#define assert(e) ((void)((e)||(RAISE(assert_failed), 0)))
-#endif
