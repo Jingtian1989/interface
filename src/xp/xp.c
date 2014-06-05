47d1aa70c9997250ec0523fbbab3e986d0eb8c83
jingtian1989 <zjt415991957@qq.com>
2014-06-05 10:05:38 +0800

    Update

---
+++ src/xp/xp.c
@@ -0,0 +1,4 @@
+#include <ctype.h>
+#include <string.h>
+#include "assert.h"
+#include "xp.h"
---
+++ src/xp/xp.h
@@ -0,0 +1,16 @@
+#ifndef __XP_H__
+#define __XP_H__
+
+#define T xp_t
+typedef unsigned char *T;
+
+extern int xp_add(int n, T z, T x, T y, int carray);
+extern int xp_sub(int n, T z, T x, T y, int borrow);
+extern int xp_mul(T z, int n, T x, int m, T y);
+extern int xp_sum(int n, T z, T x, int y);
+extern int xp_diff(int n, T z, T x, int y);
+extern int xp_product(int n, T z, T x, int y);
+extern int xp_quotient(int n, T z, T x, int y);
+extern void xp_lshift(int n, T z, int m, T x,int s, int fill);
+extern void xp_rshift(int n, T z, int m, T x, int s, int fill);
+#endif
